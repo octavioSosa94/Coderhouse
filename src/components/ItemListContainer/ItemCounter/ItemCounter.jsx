e1f2cf30748d8fd568a4e69bc5da3e2075af48a6
unknown <octaasosa@gmail.com>
2021-11-10 00:37:56 -0300

    Contador con botonv2 - Coderhouse-Sosa

--- src/components/ItemListContainer/ItemCounter/ItemCounter.jsx
+++ src/components/ItemListContainer/ItemCounter/ItemCounter.jsx
@@ -2,14 +2,15 @@ import { useState } from "react";
 import Fab from '@mui/material/Fab';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import Button from '@mui/material/Button';
 
-import FormatBoldIcon from '@mui/icons-material/FormatBold';
-import FormatItalicIcon from '@mui/icons-material/FormatItalic';
 import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
 
 
-const ItemCounter = ({ stock, initial, onAdd }) => {
+
+const ItemCounter = ({ stock, initial }) => {
     const [count, setCount] = useState(initial)
     const onIncrease = () => {
         const tmpValue = count + 1
@@ -27,6 +28,17 @@ const ItemCounter = ({ stock, initial, onAdd }) => {
             setCount(tmpValue)
 
         }
+    }
+    const showAlert = (message) => {
+        alert (message);
+
+
+    }
+    const onAdd = () => {
+
+        const message = `Agregaste ${count} producto`;
+        (count===1) ? showAlert(message) : showAlert(`${message}s`)
+
     }
 
     return (
@@ -52,9 +64,33 @@ const ItemCounter = ({ stock, initial, onAdd }) => {
           <h3>{count}</h3>
           <Divider orientation="vertical" variant="middle" flexItem />
           <AddIcon onClick = {onIncrease}/>
+          <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        width: 'fit-content',
+        border: (theme) => `1px solid ${theme.palette.divider}`,
+        borderRadius: 1,
+        bgcolor: 'background.paper',
+        color: 'text.secondary',
+        '& svg': {
+          m: 1.5,
+        },
+        '& hr': {
+          mx: 0.5,
+        },
+      }}>
+          <Button onClick = {onAdd}>
+            Agregar al carrito
+          <AddShoppingCartIcon />
+          </Button>
+          
 
       </Box>
 
+
+      </Box>
+      
     );
 
 }
